19969fe5fb30fad3911c522ea9a4940d65da6262
LidaRevtova <lida.revtova@yandex.ru>
2021-11-30 22:32:41 +0700

    сделаны контроллеры, не поддерживается валидация состояния и внешнего ключа ид проекта

--- src/main/java/application/ui/controller/TaskController.java
+++ src/main/java/application/ui/controller/TaskController.java
@@ -4,14 +4,20 @@ import application.ui.entity.Project;
 import application.ui.entity.Task;
 import application.ui.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-//@Controller
-//@RequestMapping("/{id}")   // project id
+import javax.validation.Valid;
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/project/{id}")   // project id
 public class TaskController {
 
     private final TaskRepository taskRepository;
@@ -21,37 +27,29 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
-//    @RequestMapping
-//    public ModelAndView list() {
-//        Iterable<Task> tasks = this.taskRepository.findAll();
-//        return new ModelAndView("projects/list", "tasks", tasks);
-//    }
-//
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView list(@PathVariable("id") Project project) {
+        Iterable<Task> tasks = this.taskRepository.findAll();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("task", new Task());
+        map.put("tasks", tasks);
+        map.put("project", project);
+        return new ModelAndView("tasks/add", map);
+    }
+
     @RequestMapping("{id}")
-    public ModelAndView view(@PathVariable("id") Project project) {
-        return new ModelAndView("projects/view", "project", project);
+    public ModelAndView view(@PathVariable("id") Task task) {
+        return new ModelAndView("projects/view", "task", task);
     }
 
-    @RequestMapping(params = "form", method = RequestMethod.GET)
-    public String createForm(@ModelAttribute Task task) {
-        return "projects/form";
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView create(@Valid Task task, BindingResult result,
+                               RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            return new ModelAndView("tasks/add", "formErrors", result.getAllErrors());
+        }
+        task = this.taskRepository.save(task);
+        redirect.addFlashAttribute("globalTask", "Successfully created a new task");
+        return new ModelAndView("redirect:/project/{id}/{task.id}", "task.id", task.getId());
     }
-//
-//    @RequestMapping(method = RequestMethod.POST)
-//    public ModelAndView create(@Valid Task task, BindingResult result,
-//                               RedirectAttributes redirect) {
-//        if (result.hasErrors()) {
-//            return new ModelAndView("projects/form_task", "formErrors", result.getAllErrors());
-//        }
-//        task = this.taskRepository.save(task);
-//        redirect.addFlashAttribute("globalTask", "Successfully created a new task");
-//        return new ModelAndView("redirect:/{task.taskId}", "task.taskId", task.getTaskId());
-//    }
-
-    @RequestMapping("foo")
-    public String foo() {
-        throw new RuntimeException("Expected exception in controller");
-    }
-
-
 }
--- src/main/java/application/ui/service/TaskService.java
+++ src/main/java/application/ui/service/TaskService.java
@@ -3,6 +3,8 @@ package application.ui.service;
 import application.ui.entity.Task;
 import application.ui.repository.TaskRepository;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,11 +22,22 @@ public class TaskService implements TaskRepository {
     }
 
     @Override
+    public Iterable<Task> findTasksByProjectId(int projectId) {
+        ArrayList<Task> result = new ArrayList<Task>();
+        for (Task task : tasks.values()) {
+            if (task.getProjectId().equals(projectId)) {
+                result.add(task);
+            }
+        }
+        return result;
+    }
+
+    @Override
     public Task save(Task task) {
-        Integer id = task.getTaskId();
+        Integer id = task.getId();
         if (id == null) {
             id = counter.incrementAndGet();
-            task.setTaskId(id);
+            task.setId(id);
         }
         this.tasks.put(id, task);
         return task;
