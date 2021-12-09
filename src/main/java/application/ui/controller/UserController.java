794b4b71fcc5fae62d85628abfc4dd9b352c3b35
Ilya Malakhov <malakhov.ilya.pavlovich@yandex.ru>
2021-12-09 16:21:33 +0700

    implemented email validation

--- src/main/java/application/ui/controller/UserController.java
+++ src/main/java/application/ui/controller/UserController.java
@@ -1,22 +1,18 @@
 package application.ui.controller;
 
 import application.ui.entity.User;
-import application.ui.repository.UserRepository;
 import application.ui.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.HashMap;
+import java.util.regex.Pattern;
 
 @Controller
 public class UserController {
@@ -29,6 +25,9 @@ public class UserController {
         User dbUser = UserService.getByEmail(user.getEmail());
         // TODO: hash password
         if (dbUser == null) {
+            if (!isCorrectEmail(user.getEmail())){
+                return new ModelAndView("users/login", "error", "Incorrect email");
+            }
             dbUser = UserService.create(user);
         } else if (!dbUser.getPassword().equals(user.getPassword())) {
             return new ModelAndView("users/login", "error", "Wrong password");
@@ -45,8 +44,19 @@ public class UserController {
         return new ModelAndView("users/login");
     }
 
-    private String hashPassword(String password) {
+    private static String hashPassword(String password) {
         // TODO
         return "";
+    }
+
+    private static boolean isCorrectEmail(String url){
+        String pattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+        return patternMatches(url, pattern);
+    }
+
+    private static boolean patternMatches(String string, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(string)
+                .matches();
     }
 }
